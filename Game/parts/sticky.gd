570a336258d28509ff9ff145b0bad6b8abedaf4f
Ktosox <ktoso3@gmail.com>
2024-12-09 21:15:27 +0100

    added a WIP inspector scene

--- Game/parts/sticky.gd
+++ Game/parts/sticky.gd
@@ -1,8 +1,9 @@
 extends ColorRect
 
+@export var sticks_to_group = "Paper"
 
 func _can_drop_data(at_position: Vector2, data: Variant) -> bool:
-	if data.is_in_group("Paper"):
+	if data.is_in_group(sticks_to_group):
 		return true
 	return false
 
