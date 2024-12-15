695adf9dd68e567aad703734e6a5915fd896b566
Ktosox <ktoso3@gmail.com>
2024-12-15 15:22:23 +0100

    figuring out the inspectors behavior

---
+++ Game/parts/inspector.gd
@@ -0,0 +1,32 @@
+extends Node2D
+
+signal thing_inspected (thing)
+
+var currently_held_thing : RigidBody2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+
+
+func _on_sticky_item_placed(item: RigidBody2D) -> void:
+	currently_held_thing = item
+	$Sticky.can_accept = false
+	$Label.text = currently_held_thing.name
+	pass # Replace with function body.
+
+
+func _on_inspection_button_pressed() -> void:
+	if currently_held_thing != null:
+		emit_signal("thing_inspected",currently_held_thing)
+		print("thing_inspected")
+	pass # Replace with function body.
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if body == currently_held_thing:
+		currently_held_thing = null
+		$Sticky.can_accept = true
+	pass # Replace with function body.
--- Game/parts/sticky.gd
+++ Game/parts/sticky.gd
@@ -1,11 +1,16 @@
 extends ColorRect
 
+signal item_placed (item : RigidBody2D)
+
 @export var sticks_to_group = "Paper"
+
+@export var can_accept = true
 
 func _can_drop_data(at_position: Vector2, data: Variant) -> bool:
 	if data.is_in_group(sticks_to_group):
-		return true
+		return can_accept
 	return false
 
 func _drop_data(at_position: Vector2, data: Variant) -> void:
 	data.sticky = true
+	emit_signal("item_placed",data.get_parent())
