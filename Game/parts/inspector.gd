b2d733c9a363b9eeadfbdf31fe36bb11d867fca5
Ktosox <ktoso3@gmail.com>
2024-12-26 15:13:03 +0100

    added a mockup screen for unlocked endings

--- Game/parts/inspector.gd
+++ Game/parts/inspector.gd
@@ -13,6 +13,7 @@ func _ready() -> void:
 
 func _on_sticky_item_placed(item: RigidBody2D) -> void:
 	currently_held_thing = item
+	$InspectionButton.disabled = false
 	$Sticky.can_accept = false
 	$Label.text = currently_held_thing.name
 	pass # Replace with function body.
@@ -29,4 +30,6 @@ func _on_body_exited(body: Node2D) -> void:
 	if body == currently_held_thing:
 		currently_held_thing = null
 		$Sticky.can_accept = true
+		$InspectionButton.disabled = true
+		$Label.text = ""
 	pass # Replace with function body.
